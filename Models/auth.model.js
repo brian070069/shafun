04c1436db21174a945cd4a327fb7e1d3c5c8d8e9
brian070069 <brianwgatundu@gmail.com>
2024-06-27 04:24:23 +0300

    add timestampts to user modal

--- Models/auth.model.js
+++ Models/auth.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: [true, "username is taken"],
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: [true, "username is taken"],
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: [true, "user with email already exist"],
+    },
+    password: { type: String, required: true },
+    AccountBalance: { type: Number, default: 100000 },
+    betList: [String],
   },
-  email: {
-    type: String,
-    require: true,
-    unique: [true, "user with email already exist"],
-  },
-  password: { type: String, required: true },
-  AccountBalance: { type: Number, default: 100000 },
-  betList: [String],
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
